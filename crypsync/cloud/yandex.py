49643b272b047ff449bc3b61c5ed57c999e25b25
Tivan <ivanovt1987@gmail.com>
2025-10-15 02:16:28 +0300

    Refactoring: Deleted files for new projects

--- crypsync/cloud/yandex.py
+++
@@ -1,7 +0,0 @@
-from .base import CloudStorage
-import subprocess
-
-class YandexDisk(CloudStorage):
-    def upload(self, local_file: str, remote_path: str):
-        print(f"Загружаем {local_file} на Яндекс.Диск:{remote_path}...")
-        subprocess.run(["rclone", "copy", local_file, f"yandex:{remote_path}"], check=True)
